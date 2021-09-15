ec4e7baeb8736e4bf409da134f761b5a9c57b5b7
Mayank Jha <69977369+mayankjha11@users.noreply.github.com>
2021-09-16 00:05:32 +0530

    Create Add.js

---
+++ client/src/Layout/Add/Add.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import AddItem from '../../components/AddItem/AddItem';
+
+const Add = () => {
+  return (
+    <Fragment>
+      <AddItem />
+    </Fragment>
+  );
+};
+
+export default Add;
