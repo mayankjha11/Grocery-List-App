187d4640ad5ec664bedf61ea3672c9c77b063d64
Mayank Jha <69977369+mayankjha11@users.noreply.github.com>
2021-09-16 01:03:06 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = "mongodb://localhost:27017/db_test";
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('cors')());
+app.use(require('helmet')());
+app.use('/api/items', require('./routes/items'));
+
+// Production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
